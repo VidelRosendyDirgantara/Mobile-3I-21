e3c3af176ff1cb8e1ed17c1fef21c9d5a6b44fef
VidelRosendyDirgantara <videl2706@gmail.com>
2021-09-24 00:52:35 +0700

    Menambah Praktikum 7 - B1

--- src/BasicUI_A1/app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ src/BasicUI_A1/app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -113,4 +113,11 @@ public class MainActivity extends AppCompatActivity {
             return f.format(val);
         }
     }
+
+    public void doConvert() {
+        RadioButton selected = (RadioButton) findViewById(unitType.getCheckedRadioButtonId());
+        double val = Double.parseDouble(inputTxt.getText().toString());
+        double res = convertUnit(selected.getText().toString(), unitOri.getSelectedItem().toString(), unitConv.getSelectedItem().toString(), val);
+        outputTxt.setText(strResult(res, roundBox.isChecked()));
+    }
 }

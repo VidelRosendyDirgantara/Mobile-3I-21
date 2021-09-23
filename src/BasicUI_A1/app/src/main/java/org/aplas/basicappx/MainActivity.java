c5c453615a9419d41b113503eb0a7b76abd80559
VidelRosendyDirgantara <videl2706@gmail.com>
2021-09-23 22:57:32 +0700

    Menambah Praktikum 5 - B1

--- src/BasicUI_A1/app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ src/BasicUI_A1/app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -1,8 +1,10 @@
 package org.aplas.basicappx;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,6 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        convertBtn = (Button) findViewById(R.id.convertButton);
+        inputTxt = (EditText) findViewById(R.id.inputText);
+        outputTxt = (EditText) findViewById(R.id.outputText);
+        unitOri = (Spinner) findViewById(R.id.oriList);
+        unitConv = (Spinner) findViewById(R.id.convList);
+        unitType = (RadioGroup) findViewById(R.id.radioGroup);
+        roundBox = (CheckBox) findViewById(R.id.chkRounded);
+        formBox = (CheckBox) findViewById(R.id.chkFormula);
+        imgView = (ImageView) findViewById(R.id.img);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startDialog = new AlertDialog.Builder(MainActivity.this).create();
+        startDialog.setTitle("Application started");
+        startDialog.setMessage("This app can use to convert any units");
+        startDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        startDialog.show();
     }
 
     private Distance dist = new Distance();
@@ -32,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox roundBox;
     private CheckBox formBox;
     private ImageView imgView;
+    private AlertDialog startDialog;
 
     protected double convertUnit (String type, String oriUnit, String convUnit,double value) {
         if (type.equals("Temperature")) {

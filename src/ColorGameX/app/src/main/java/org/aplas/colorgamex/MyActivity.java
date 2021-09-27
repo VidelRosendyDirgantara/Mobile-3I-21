161584a6ba243d4dd9abb0e475edd4666be09eb1
VidelRosendyDirgantara <videl2706@gmail.com>
2021-09-28 01:44:38 +0700

    Perbaikan Praktikum 7 - B2

--- src/ColorGameX/app/src/main/java/org/aplas/colorgamex/MyActivity.java
+++ src/ColorGameX/app/src/main/java/org/aplas/colorgamex/MyActivity.java
@@ -13,8 +13,10 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 public class MyActivity extends AppCompatActivity {
     TextView timer;
@@ -35,6 +37,7 @@ public class MyActivity extends AppCompatActivity {
     CountDownTimer countDown;
     String[] clrList;
     HashMap charList = new HashMap();
+    boolean isStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +90,13 @@ public class MyActivity extends AppCompatActivity {
     }
 
     public void startGame(View v) {
-
+        if (!isStarted) {
+            progress.setProgress(0);
+            scoreText.setText("0");
+            start.setVisibility(View.INVISIBLE);
+            isStarted = true;
+            newGameStage();
+        }
     }
 
     public void submitColor(View v) {
@@ -116,4 +125,24 @@ public class MyActivity extends AppCompatActivity {
             charList.put(clrList[i], temp[i]);
         }
     }
+
+    int getNewRandomInt(int min, int max, int except) {
+        Random r = new Random();
+        boolean found = false;
+        int number;
+        do {
+            number = r.ints(min, (max + 1)).findFirst().getAsInt();
+            if (number!=except) found=true;
+        } while (!found);
+        return number;
+    }
+
+    private void newGameStage() {
+        String clrTxt = ((TextView) findViewById(R.id.clrText)).getText().toString();
+        int lastNum = Arrays.asList(clrList).indexOf(clrTxt);
+        int colorIdx = getNewRandomInt(0, 5, lastNum);
+        clrText.setText(clrList[colorIdx]);
+        countDown.start();
+    }
+
 }

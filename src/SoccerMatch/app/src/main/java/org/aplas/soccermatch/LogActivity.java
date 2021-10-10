9b9bb2e804da10a65426f3ba1bface68c5effc7b
VidelRosendyDirgantara <videl2706@gmail.com>
2021-10-10 14:09:40 +0700

    Menambah Praktikum 7 - B3

--- src/SoccerMatch/app/src/main/java/org/aplas/soccermatch/LogActivity.java
+++ src/SoccerMatch/app/src/main/java/org/aplas/soccermatch/LogActivity.java
@@ -1,8 +1,14 @@
 package org.aplas.soccermatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class LogActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogActivity();
+            }
+        });
     }
 }

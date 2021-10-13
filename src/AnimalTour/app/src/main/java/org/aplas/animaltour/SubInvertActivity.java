f54571f4b76d950d632227397db08e94f3e45da1
VidelRosendyDirgantara <videl2706@gmail.com>
2021-10-13 19:37:06 +0700

    Menambah Praktikum 5

---
+++ src/AnimalTour/app/src/main/java/org/aplas/animaltour/SubInvertActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.animaltour;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubInvertActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_invert);
+    }
+}

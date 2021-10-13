a2633b343727c285122c9157c7b986c14e48debc
VidelRosendyDirgantara <videl2706@gmail.com>
2021-10-13 19:00:55 +0700

    Menambah Praktikum 3

---
+++ src/AnimalTour/app/src/main/java/org/aplas/animaltour/InvertActivity.java
@@ -0,0 +1,18 @@
+package org.aplas.animaltour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
+
+public class InvertActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invert);
+    }
+}

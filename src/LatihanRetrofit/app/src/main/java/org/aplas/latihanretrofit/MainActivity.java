c39d8674e8f54e684e79207e6d1f0ff5da0bce5c
mashhisyam <haryoh48@gmail.com>
2021-12-07 13:46:41 +0700

    mengubah activity main dan menambah gradle activity main bunding

--- src/LatihanRetrofit/app/src/main/java/org/aplas/latihanretrofit/MainActivity.java
+++ src/LatihanRetrofit/app/src/main/java/org/aplas/latihanretrofit/MainActivity.java
@@ -1,14 +1,64 @@
 package org.aplas.latihanretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.aplas.latihanretrofit.models.Repo;
+import org.aplas.latihanretrofit.services.GithubService;
+import org.aplas.latihanretrofit.databinding.ActivityMainBinding;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private GithubService service;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(GithubService.class);
+
+        binding.setUsername("VidelRosendyDirgantara");
+    }
+    public void handlesend(View view) {
+        Call<List<Repo>> repositoryCall = service.listRepos(binding.getUsername());
+
+        repositoryCall.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                List<Repo> results = response.body();
+                if(results != null && response.isSuccessful()) {
+                    binding.setRepo(results.get(0));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                Log.e("Error", t.getMessage());
+            }
+        });
+
     }
 }
